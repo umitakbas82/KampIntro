f44aeca74d8593adc793cad6e964516aa6d0ddb4
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-11-04 22:50:59 +0300

    OOP3

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager)
+        {
+            //Basvuran Bilgilerini Değerlendirme
+            krediManager.Hesapla();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
